12357895cffab978e952650f29de3387977719b7
greencowpat <father_harris@hotmail.com>
2021-11-28 23:03:45 +1100

    added operator classes, highlight operator on press and decimal

--- js/calculator.js
+++ js/calculator.js
@@ -10,13 +10,52 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 };
+const calculator = document.querySelector(".calculator")
 const calcOutput = document.querySelector('#calcOutput p');
 
-const digit = document.querySelectorAll('.digit button')
-const btn = document.querySelectorAll('button')
+const btn = document.querySelectorAll('button');
+const keypad = document.querySelector('.numberpad');
+
 
 btn.forEach(function(button) {
     button.addEventListener("click", (e) => {
-        console.log(e.target)
+        
     })
-})
+}) 
+
+keypad.addEventListener("click", e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const displayedNum = calcOutput.textContent;
+        const keycontent = key.textContent;
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+
+        if (
+            action === "add" ||
+            action === "subtract" || 
+            action === "multiply" ||
+            action === "divide"
+        ) {
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+            key.classList.add('is-depressed')
+        
+
+        
+        }
+        if (!action) {
+            if (displayedNum === '0') {
+                calcOutput.textContent = keycontent;
+            } else {
+                calcOutput.textContent += keycontent;
+            }
+            console.log(typeof calcOutput.textContent)
+        }
+        if (action === "decimal" && (!calcOutput.textContent.includes('.'))) {
+            calcOutput.textContent += '.'; 
+        }
+    };
+});
