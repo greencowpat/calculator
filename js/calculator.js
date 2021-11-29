0fd449b227521ba8b0253aaf4811832e3295b2fd
greencowpat <father_harris@hotmail.com>
2021-11-29 22:59:13 +1100

    calculator functional for 'best use case'. need to fix for edge cases

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,3 @@
-const addition = (a, b) => {
-    return a + b;
-};
-const subtraction = (a, b) => {
-    return a - b;
-};
-const multiply = (a, b) => {
-    return a * b;
-};
-const divide = (a, b) => {
-    return a / b;
-};
 const calculator = document.querySelector(".calculator")
 const calcOutput = document.querySelector('#calcOutput p');
 
@@ -17,11 +5,27 @@ const btn = document.querySelectorAll('button');
 const keypad = document.querySelector('.numberpad');
 
 
-btn.forEach(function(button) {
-    button.addEventListener("click", (e) => {
-        
-    })
-}) 
+
+const calculate = (n1, operator, n2) => {
+    if (operator === "add") {
+        return parseFloat(n1) + parseFloat(n2);
+    } 
+    
+    if (operator === "subtract") {
+        return parseFloat(n1) - parseFloat(n2);
+    } 
+
+    if (operator === 'multiply') {
+        return parseFloat(n1) * parseFloat(n2);
+    }
+
+    if (operator === 'divide') {
+        return parseFloat(n1) / parseFloat(n2);
+    };
+
+    return result;
+};
+
 
 keypad.addEventListener("click", e => {
     if (e.target.matches('button')) {
@@ -29,6 +33,7 @@ keypad.addEventListener("click", e => {
         const action = key.dataset.action;
         const displayedNum = calcOutput.textContent;
         const keycontent = key.textContent;
+        let previousKeyType = calculator.dataset.previousKeyType;
 
         Array.from(key.parentNode.children)
             .forEach(k => k.classList.remove('is-depressed'))
@@ -41,21 +46,40 @@ keypad.addEventListener("click", e => {
         ) {
             calculator.dataset.firstValue = displayedNum;
             calculator.dataset.operator = action;
-            key.classList.add('is-depressed')
-        
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = "operator";
+    
+        };
 
-        
-        }
+
         if (!action) {
-            if (displayedNum === '0') {
+            if (displayedNum === '0' || previousKeyType === "operator") {
                 calcOutput.textContent = keycontent;
             } else {
-                calcOutput.textContent += keycontent;
-            }
-            console.log(typeof calcOutput.textContent)
-        }
+                calcOutput.textContent = displayedNum + keycontent;
+            };
+            calculator.dataset.previousKeyType = 'number';
+        };
+        
         if (action === "decimal" && (!calcOutput.textContent.includes('.'))) {
             calcOutput.textContent += '.'; 
-        }
+            calculator.dataset.previousKey.Type = 'decimal';
+        };
+
+        if (action === "clear") {
+            console.log('clear!')
+            calculator.dataset.previousKeyType = 'clear';
+        };
+
+        if (action === "operate") {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            calcOutput.textContent = calculate(firstValue, operator, secondValue);
+            calculator.dataset.previousKeyType = 'calculate';
+        };
+
+        console.log(previousKeyType);
     };
 });
